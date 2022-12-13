c707d3b9e75c336b38ead48d7b7f6bb4556716c4
Alessandro <defranceschi_a@yahoo.com>
2022-12-13 01:31:33 +0100

    graphics fix for background

--- revamp-dapp/src/components/Layout/Layout.tsx
+++ revamp-dapp/src/components/Layout/Layout.tsx
@@ -4,18 +4,20 @@ import { useLocation } from "react-router-dom";
 import { routes, routeNames } from "routes";
 import { Footer } from "./Footer";
 import { Navbar } from "./Navbar";
-import ElementalBg from "./../../assets/img/bg.png";
+import ElementalBg from "/bg.png";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
 	const { search } = useLocation();
 	return (
-		<div className="bg-light d-flex flex-column flex-fill wrapper">
+		<div
+			className="bg-light d-flex flex-column flex-fill wrapper"
+			style={{
+				backgroundImage: `url(/bg.png)`,
+				backgroundSize: "cover",
+			}}
+		>
 			<Navbar />
-			<div
-				style={{
-					backgroundImage: `url(${ElementalBg})`,
-				}}
-			>
+			<div>
 				<main className="d-flex flex-column flex-grow-1">
 					<AuthenticatedRoutesWrapper
 						routes={routes}
